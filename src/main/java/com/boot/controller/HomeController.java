b6cbca4c5871cdca1aea16900958177ac379a0d8
Evans-Munatsa <evansmunatsa7@gmail.com>
2017-08-12 10:20:31 +0200

    added the first routes

--- src/main/java/com/boot/controller/HomeController.java
+++ src/main/java/com/boot/controller/HomeController.java
@@ -1,13 +1,16 @@
-package controller;
+package com.boot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * EvansMunatsa 12/08/2017
+ *
+ */
 @RestController
 public class HomeController {
 	@RequestMapping("/")
 	public String home() {
 		return "Das Boot, reporting for duty!";
 	}
-
 }
